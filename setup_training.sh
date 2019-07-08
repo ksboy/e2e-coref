2e71709f3b246275813fbbf665244cdb021ec55b
ksboy <weihouks@gmail.com>
2019-07-08 15:03:04 +0800

    chage to mac

--- setup_training.sh
+++ setup_training.sh
@@ -7,17 +7,17 @@ dlx() {
 }
 
 conll_url=http://conll.cemantix.org/2012/download
-dlx $conll_url conll-2012-train.v4.tar.gz
-dlx $conll_url conll-2012-development.v4.tar.gz
-dlx $conll_url/test conll-2012-test-key.tar.gz
-dlx $conll_url/test conll-2012-test-official.v9.tar.gz
+# dlx $conll_url conll-2012-train.v4.tar.gz
+# dlx $conll_url conll-2012-development.v4.tar.gz
+# dlx $conll_url/test conll-2012-test-key.tar.gz
+# dlx $conll_url/test conll-2012-test-official.v9.tar.gz
 
-dlx $conll_url conll-2012-scripts.v3.tar.gz
+# dlx $conll_url conll-2012-scripts.v3.tar.gz
 
-dlx http://conll.cemantix.org/download reference-coreference-scorers.v8.01.tar.gz
-mv reference-coreference-scorers conll-2012/scorer
+# dlx http://conll.cemantix.org/download reference-coreference-scorers.v8.01.tar.gz
+# mv reference-coreference-scorers conll-2012/scorer
 
-ontonotes_path=/projects/WebWare6/ontonotes-release-5.0
+ontonotes_path=./ontonotes-release-5.0
 bash conll-2012/v3/scripts/skeleton2conll.sh -D $ontonotes_path/data/files/data conll-2012
 
 function compile_partition() {
